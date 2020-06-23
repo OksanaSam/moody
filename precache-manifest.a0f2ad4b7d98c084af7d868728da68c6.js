7483ba272d46ea9feaacf9d1ef7eb5e43d6f8f70
oksana sam <oksana.o.samokhvalova@gmail.com>
2020-06-23 12:14:00 -0400

    Updates

---
+++ precache-manifest.a0f2ad4b7d98c084af7d868728da68c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e5b29c4c84ceb478a8945fe07bc972",
+    "url": "/oksana-samokhvalova-project-five/index.html"
+  },
+  {
+    "revision": "6761d8c6e08173cdd3a4",
+    "url": "/oksana-samokhvalova-project-five/static/css/main.e14929af.chunk.css"
+  },
+  {
+    "revision": "38879a68a4f9aa8283e8",
+    "url": "/oksana-samokhvalova-project-five/static/js/2.878a1045.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oksana-samokhvalova-project-five/static/js/2.878a1045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6761d8c6e08173cdd3a4",
+    "url": "/oksana-samokhvalova-project-five/static/js/main.a5436166.chunk.js"
+  },
+  {
+    "revision": "af45eea21503439c8ce7",
+    "url": "/oksana-samokhvalova-project-five/static/js/runtime-main.707f4e67.js"
+  },
+  {
+    "revision": "12a0f127634f56ceb7eec05e228619b7",
+    "url": "/oksana-samokhvalova-project-five/static/media/logo.12a0f127.png"
+  }
+]);
